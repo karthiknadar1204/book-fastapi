a8b98cb3175666d384bc5f4d2518414c40398f77
karthik nadar <karthiknadar1204@gmail.com>
2025-12-12 19:59:49 +0530

    path parameters

--- book/src/book_api/main.py
+++ book/src/book_api/main.py
@@ -7,6 +7,10 @@ app=FastAPI()
 async def read_root():
     return {"message": "Hello, World!"}
 
+@app.get("/greet/{name}")
+async def greet_name(name:str)->dict:
+    return {"message":f"Hello, {name}!"}
+
 def start():
     """Start the FastAPI server."""
     uvicorn.run("book_api.main:app", host="127.0.0.1", port=8080, reload=True)
